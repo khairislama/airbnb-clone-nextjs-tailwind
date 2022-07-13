1801347655bff62f67b36384f00d04a142d5e76c
khairislama <khairi.slama.1996.br@gmail.com>
2022-07-13 17:13:28 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Khairi | Airbnb clone with nextjs and tailwindcss</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/** Header */}
+      {/** Banner */}
+    </div>
+  );
+};
+
+export default Home;
