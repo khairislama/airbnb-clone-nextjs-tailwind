2dc785eb7a73235a3186fb706316038a59de375b
khairislama <khairi.slama.1996.br@gmail.com>
2022-07-14 14:41:41 +0200

    index added

---
+++ components/Footer.tsx
@@ -0,0 +1,39 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb Works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not the real site</p>
+        <p>It's just a cool clone</p>
+        <p>Referrals accepted</p>
+        <p>To my CV</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Khairi react</p>
+        <p>presents</p>
+        <p>An Airbnb clone</p>
+        <p>To practice tailwindcss</p>
+        <p>and next js</p>
+      </div>
+      <div className="space-y-4 text-sm text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & safety</p>
+        <p>Say Hi Youtube</p>
+        <p>Easter Eggs</p>
+        <p>For the win</p>
+      </div>
+    </div>
+  );
+}
+export default Footer;
